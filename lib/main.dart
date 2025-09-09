26124511ac8b8198e2abece38db08923ebe0e674
unknown <addimanzerihun@gmail.com>
2025-09-09 10:49:23 +0300

    change folder name and some change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map_integration/presentations/street_map.dart';
+import 'package:flutter_map_integration/presentations/street_map-screen.dart';
 
 
 void main() {
