fb1c0c42a32d5586deca0f5e497272c16cdb3397
unknown <addimanzerihun@gmail.com>
2025-09-09 09:26:33 +0300

    feat: show flutter map

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
-  runApp( MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: StreetMap(),
   ));
@@ -15,16 +18,47 @@ class StreetMap extends StatefulWidget {
 }
 
 class _StreetMapState extends State<StreetMap> {
+  MapController _mapController = MapController();
+
+  @override
+  void dispose() {
+    _mapController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      appBar:AppBar(
+      appBar: AppBar(
         title: Text('Street Map'),
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
+      body: Stack(
+        children: [
+          FlutterMap(
+              mapController: _mapController,
+              options: MapOptions(
+                initialCenter: const LatLng(0, 0),
+                minZoom: 3,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  //userAgentPackageName: 'com.example.flutter_map_integration',
+                ),
+                CurrentLocationLayer(
+                  style: const LocationMarkerStyle(
+                    marker: DefaultLocationMarker(
+                      child: Icon(Icons.location_pin,color: Colors.white,),
+                    ),
+                    markerSize: Size(35, 35),
+                    markerDirection: MarkerDirection.heading
+                  ),
+                ),
+              ]),
+        ],
+      ),
     );
   }
 }
-
